517fd7a98b0465cbdf6856f668386fe96aa7114b
Ion Petrache <ionpetrache@socialsweethearts.de>
2017-12-19 19:46:38 +0100

    Greet new user and aler others

--- server/server.js
+++ server/server.js
@@ -16,13 +16,31 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (msg) => {
         console.log('createMessage', msg);
         io.emit('newMessage', {
             from: msg.from,
             text: msg.text,
             createdAt: new Date().getTime()
-        })
+        });
+
+        socket.broadcast.emit('newMessage', {
+            from: msg.from,
+            text: msg.text,
+            createAt: new Date().getTime()
+        });
     });
 
 
